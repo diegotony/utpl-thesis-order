cb88d0c9fef9e1a4076a49fcf6808676b53f7829
tucotony <tucotony1396@gmail.com>
2019-09-25 18:06:25 -0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import config from './config/config';
 import * as helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors:true});
+  const app = await NestFactory.create(AppModule, {cors: true});
   app.use(helmet());
   await app.listen(config.PORT);
 }
