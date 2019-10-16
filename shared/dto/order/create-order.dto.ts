090c326ea6788ffe8982b6815091095163d7cbe4
diegotony <tucotony1396@gmail.com>
2019-10-16 11:54:22 -0500

    swagger updated

--- shared/dto/order/create-order.dto.ts
+++ shared/dto/order/create-order.dto.ts
@@ -1,11 +1,19 @@
+import { ApiModelProperty } from "@nestjs/swagger";
 
 
 export class CreateOrderDto {
+    @ApiModelProperty()
     readonly _id: string;
+    @ApiModelProperty()
     readonly table: string;
+    @ApiModelProperty()
     readonly id_user: string;
+    @ApiModelProperty()
     readonly ids: Array<String>;
+    @ApiModelProperty()
     readonly prices: Array<Number>;
+    @ApiModelProperty()
     readonly quantitys: Array<Number>;
+    @ApiModelProperty()
     readonly date: Date;
 }
