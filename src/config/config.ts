985fbc7b0aad243d13b2f165e6f260bf42fdd830
diegotony <tucotony1396@gmail.com>
2019-10-16 00:44:44 -0500

    envs updated

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 export default {
     MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/order',
     REDIS_URI: process.env.REDIS_URI || 'redis://localhost:6379',
-    PORT: process.env.PORT || '1396',
+    PORT: process.env.PORT || '3037',
 };
