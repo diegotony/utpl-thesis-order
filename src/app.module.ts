bc4faf96d8915262a8e978ea90d307ec07453942
diegotony <tucotony1396@gmail.com>
2019-11-08 00:30:51 -0500

    added helth checks

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,29 @@
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
-import { ItemModule } from './item/item.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { CatalogModule } from './catalog/catalog.module';
-import { TableModule } from './table/table.module';
-import config from './config/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { OrderModule } from './order/order.module';
-import { LoggerMiddleware } from './logger.middleware';
-
+import { Module, NestModule, MiddlewareConsumer } from "@nestjs/common";
+import { ItemModule } from "./item/item.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { CatalogModule } from "./catalog/catalog.module";
+import { TableModule } from "./table/table.module";
+import config from "./config/config";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { OrderModule } from "./order/order.module";
+import { LoggerMiddleware } from "./logger.middleware";
+import { TerminusModule } from "@nestjs/terminus";
+import { TerminusOptionsService } from "./services/terminus-options/terminus-options.service";
 
 @Module({
-imports: [MongooseModule.forRoot("mongodb://"+config.MONGO_HOST+"/"+config.MONGO_DB, {useNewUrlParser: true }), ItemModule, CatalogModule, TableModule,OrderModule
-],
+  imports: [
+    MongooseModule.forRoot(
+      "mongodb://" + config.MONGO_HOST + "/" + config.MONGO_DB,
+      { useNewUrlParser: true }
+    ),
+    ItemModule,
+    CatalogModule,
+    TableModule,
+    OrderModule,
+    TerminusModule.forRootAsync({useClass: TerminusOptionsService})
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
-export class AppModule{}
+export class AppModule {}
