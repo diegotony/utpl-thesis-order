6c3ae9ae7de9a8303d8edfe42740ec37c8d35310
diegotony <tucotony1396@gmail.com>
2020-01-17 01:13:33 -0500

    server status

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,14 +2,13 @@ import { Controller, Get, HttpCode } from "@nestjs/common";
 import { AppService } from "./app.service";
 import { ApiUseTags } from "@nestjs/swagger";
 
-
 @ApiUseTags()
 @Controller()
-export class AppController{
-  constructor(private readonly appService: AppService){} 
+export class AppController {
+  constructor(private readonly appService: AppService) {}
   @Get()
   @HttpCode(200)
-  async hello(){
-    return ( await this.appService.hello())
+  async hello() {
+    return { service: { status: true } };
   }
 }
